6c3fbf8321fcfb628ebd078f43efe5ee9ecc52ba
Arnaldo Victor Yamawaki Alves <43183787+Yamwaki-kun@users.noreply.github.com>
2019-01-17 16:33:45 -0200

    Update up.sh

--- up.sh
+++ up.sh
@@ -6,27 +6,37 @@ echo
 	sudo apt update
 echo
 echo Listas Atualizadas
+sleep 5
+clear
 
 echo Atualizando Os Softwares
 echo
 	sudo apt -y upgrade
 echo
 echo Softwares Atualizadas
+sleep 5
+clear
 
 echo Atualizando O Kernel
 echo
 	sudo apt -y dist-upgrade
 echo
 echo Kernel Atualizados
+sleep 5
+clear
 
 echo Removendo Softwares
 echo
 	sudo apt -y autoremove
 echo
 echo Softwares Removidos
+sleep 5
+clear
 
 echo Removendo Cache
 echo
 	sudo apt -y autoclean
 echo
 echo Cache Removido
+sleep 5
+clear
